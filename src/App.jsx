5d8027fff7c42a128b124c2a86f36a1ffcacac89
Om-Sureja <omsureja@gmail.com>
2024-01-10 15:00:03 +0530

    Weather app is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,64 @@
-import { useState } from 'react'
-import './App.css'
-
-
-
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [city, setCity] = useState("london");
+  const [data,setData]=useState([])
+  
 
-  const[city,setCity]=useState("")
-  const[weatherData,setWeatherData]=useState(null);
-  const[error,setError]=useState(null)
+    useEffect(()=>{
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a0e1806263ce421e88981800240301&q=${city}&aqi=no`);
+          const data = await response.json();
+          setData(data)
+          console.log(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      fetchData()
+     
+    },[city])
 
-
-  function fetchData(){
-    fetch(`http://api.weatherapi.com/v1/current.json?key=a0e1806263ce421e88981800240301&q=${city}&aqi=yes`)
-    .then(async function(res){
-      const data=await res.json();
-      if(!res.ok){
-        console.log("error")
-        setError("error")
-      }
-      else{
-        setWeatherData(data.current)
-        setError(null)
-      }
-    })
-  }
 
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", objectFit:"fit"}}>
-      <div style={{ textAlign: "center" }}>
-        <div>
-          <input style={{ height: "30px", width: "200px", borderRadius: "20px" }} type='text' placeholder='   Enter Your City' onChange={function (e) {
-            const value = e.target.value
-            setCity(value);
-          }}></input>
-          <button style={{ marginLeft: "20px", height: "30px", width: "150px", borderRadius: "20px" }} onClick={()=>{
-            // setCity(value),
-            fetchData()
-          }}>Get Weather</button>
-        </div>
-
+    < div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',background:'black', height:'100vh' ,width:'100%'}}>
+      <div style={{textAlign:'center'}}>
+        <input style={{textAlign:'center',color:'white',boxShadow: '0 0 10px 2px white', animation: 'glow 2s infinite',border:'none',borderRadius:'20px',height:'30px', width:'300px',background:'black'}}
+          type='text'
+          placeholder='     Enter Your City'
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        /><br>
+        </br>
         
-        {error ? <div>No City Found</div> : 
-          <div style={{ borderRadius: "10px", background: "black", color: "white", width: "500px", height: "500px" ,backgroundImage:`url(${weatherData.condition.icon})`,backgroundSize:"cover",backgroundPosition:"center"}} >
-            <h3 style={{ fontSize: "30px", paddingTop: "20px", marginLeft: "50px", display: "flex", alignContent: "flex-start" }}>{city}</h3>
-            <h1 style={{ fontSize: "80px", marginTop: "-20px", marginLeft: "50px", display: "flex", alignContent: "flex-start" }}>{weatherData.temp_c}°C</h1>
-  
-            <p style={{ fontSize: "17px", paddingTop: "120px", marginLeft: "50px", display: "flex", alignContent: "flex-start" }}> Today's weather is {weatherData.condition.text} in {city}</p>
-  
-            <div style={{ borderRadius: "50px", marginLeft: "50px", display: 'flex', justifyContent: "space-evenly", background: "grey", width: "400px" }}>
-              <div>
-                <p>Wind </p>
-                <p>{weatherData.wind_kph} kph</p>
-              </div>
-              <div>
-                <p>Precipitation</p>
-                <p>{weatherData.precip_mm} mm</p>
-              </div>
-              <div>
-                <p>Humidity</p>
-                <p>{weatherData.humidity} %</p>
-              </div>
-              <div>
-                <p>Air Quality</p>
-                <p>{weatherData.air_quality.co}</p>
-              </div>
+
+        <div style={{boxShadow: '0 0 10px 2px white', animation: 'glow 2s infinite' ,marginTop:'50px',paddingTop:'15px',border:'1px solid grey',borderRadius:'15px',background:'black', height:'500px' ,width:"400px",color:'white'}}>
+          {data.current ? <p>{data.current.last_updated}</p> : null}
+          {data.current ? <h1>{data.current.temp_c} °C</h1> : null}
+          {data.location ? <p style={{paddingTop:'40px'}}>{data.location.name}</p> : null}
+          {data.current ? <p style={{color:'grey'}}>{data.current.condition.text}</p> : null}
+          <div style={{paddingTop:'120px',display:'flex', justifyContent:'space-evenly'}}>
+            <div>
+              {data.current ? <p>{data.current.humidity}%</p> : null }
+              <p>Humidity</p>
+            </div>
+            <div>
+              {data.current ? <p>{data.current.wind_kph} km/h</p> : null}
+              <p>Wind</p>
+            </div>
+            <div>
+              {data.current ? <p>{data.current.precip_mm}mm</p> : null}
+              <p>Rain</p>
             </div>
           </div>
-          }
-        
+        </div>
       </div>
     </div>
-  )
-  
+  );
 }
 
-export default App
+export default App;
