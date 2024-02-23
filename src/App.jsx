025f7335d1c6a17abd3333609d4c03dc1281e222
Om-Sureja <omsureja@gmail.com>
2024-02-23 20:28:30 +0530

    speed and analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [city, setCity] = useState("delhi");
